424db3420bd5ea23d4701d837613139294519401
RobertKSuter <RKS77@med.miami.edu>
2025-11-07 13:40:10 -0500

    scFOCAL refactor

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' LINCS.ResponseSigs
+#'
+#' 2017 Release TCSs as published in Stathias et al., 2018
+#'
+#' @format A data frame with 1679 rows and 961 variables:
+#' \describe{
+#'   \item{id}{The unique identifier}
+#'   \item{name}{The name associated with the id}
+#' }
+#' @source \url{http://www.source-of-my-data.com}
+"LINCS.ResponseSigs"
+
+#' L1000_compounds
+#'
+#' Compounds included in 2017 Release TCSs as published in Stathias et al., 2018
+#'
+#' @format A character vector with 1679 elements.
+#' \describe{
+#'   \item{id}{The unique identifier}
+#'   \item{name}{The name associated with the id}
+#' }
+#' @source \url{http://www.source-of-my-data.com}
+"L1000_compounds"
+
+#' L1000_genes
+#'
+#' character vector of L1000-measured genes
+#'
+#' @format chr vector 961 elements
+#' \describe{
+#'   \item{id}{The unique identifier}
+#'   \item{name}{The name associated with the id}
+#' }
+#' @source \url{http://www.source-of-my-data.com}
+"L1000_genes"
